8232b790c43c11b98b379ef658958405cb8b4ad5
J. Ametisto <139443058+ametisto26@users.noreply.github.com>
2023-07-22 04:32:43 +0900

    create checkingTypeOfVar.py

---
+++ checkingTypeOfVar.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+型の確認
+"""
+import numpy as np
+a = 6
+b = np.pi
+c = "Pi"
+d = True
+
+print(f'type(a) = {type(a)}')
+print(f'type(b) = {type(b)}')
+print(f'type(c) = {type(c)}')
+print(f'type(d) = {type(d)}')
