364f265365cb1a95542866cb5bc6721ec9731b3f
J. Ametisto <139443058+ametisto26@users.noreply.github.com>
2023-07-22 05:19:04 +0900

    update checkingTypeOfVar.py

--- checkingTypeOfVar.py
+++ checkingTypeOfVar.py
@@ -7,8 +7,16 @@ a = 6
 b = np.pi
 c = "Pi"
 d = True
+e = ["a", "b"]
+f = ("a", "b")
+g = {"name": "Kristalo", "age": 888}
+h = {"a", "b"}
 
 print(f'type(a) = {type(a)}')
 print(f'type(b) = {type(b)}')
 print(f'type(c) = {type(c)}')
 print(f'type(d) = {type(d)}')
+print(f'type(e) = {type(e)}')
+print(f'type(f) = {type(f)}')
+print(f'type(g) = {type(g)}')
+print(f'type(h) = {type(h)}')
