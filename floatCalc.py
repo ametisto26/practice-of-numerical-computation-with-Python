9e4973fcaefb580f8386e2e60a8ece434f961926
J. Ametisto <139443058+ametisto26@users.noreply.github.com>
2023-07-27 07:10:53 +0900

    create floatCalc.py

---
+++ floatCalc.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+math module
+"""
+import math
+
+a = input('a = ')
+b = input('b = ')
+a, b = float(a), float(b)
+
+print('a + b = ', a + b)
+print('a - b = ', a - b)
+print('a * b = ', a * b)
+print('a / b = ', a / b)
+
+
+print('sqrt(a) = ', math.sqrt(a))
+print('sqrt(b) = ', math.sqrt(b))
+
+
+print('exp(a) = ', math.exp(a))
+
+print('sin(a) = ', math.sin(a))
+
+print('tan(a) = ', math.tan(a))
+
+print('log(a) = ', math.log(a))
+
+print('log10(a) = ', math.log10(a))
+
+print('atan(a) = ', math.atan(a))
