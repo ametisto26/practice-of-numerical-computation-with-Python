5d81d45176c19ad4cab4fd411dd0df914c92925f
J. Ametisto <139443058+ametisto26@users.noreply.github.com>
2023-07-27 11:52:17 +0900

    create complexfunc.py

---
+++ complexFunc.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+複素関数
+"""
+import cmath
+
+
+a = 1.2 + 3.4j
+
+
+print('sqrt(a) = ', cmath.sqrt(a))
+
+print('exp(a) = ', cmath.exp(a))
+
+print('sin(a) = ', cmath.sin(a))
+
+print('tan(a) = ', cmath.tan(a))
+
+print('log(a) = ', cmath.log(a))
+
+print('log10(a) = ', cmath.log10(a))
+
+print('atan(a) = ', cmath.atan(a))
+
+
